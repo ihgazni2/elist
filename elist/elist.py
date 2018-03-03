99da3bd4bb251a93103b1bc515ac8dd8deca7e0f
ihgazni2 <286264978@qq.com>
2018-03-03 22:22:58 +0800

    remove old build

--- elist/elist.py
+++ elist/elist.py
@@ -3623,9 +3623,7 @@ def fullfill_descendants_info(desc_matrix):
     '''
     pathloc_mapping = {}
     locpath_mapping = {}
-    #def leaf_handler(desc,pdesc,offset):
     def leaf_handler(desc,pdesc):
-        #desc['flat_offset'] = (offset,offset+1)
         desc['non_leaf_son_paths'] = []
         desc['leaf_son_paths'] = []
         desc['non_leaf_descendant_paths'] = []
@@ -3635,9 +3633,7 @@ def fullfill_descendants_info(desc_matrix):
             pdesc['flat_len'] = pdesc['flat_len'] + 1
         else:
             pdesc['flat_len'] = 1
-    #def non_leaf_handler(desc,pdesc,offset):
     def non_leaf_handler(desc,pdesc):
-        #desc['flat_offset'] = (offset,offset+desc['flat_len'])
         pdesc['non_leaf_descendant_paths'].extend(copy.deepcopy(desc['non_leaf_descendant_paths']))
         pdesc['leaf_descendant_paths'].extend(copy.deepcopy(desc['leaf_descendant_paths']))
         if(pdesc['flat_len']):
@@ -3654,16 +3650,12 @@ def fullfill_descendants_info(desc_matrix):
     desc_level = desc_matrix[depth - 1]
     length = desc_level.__len__()
     #the last level
-    #offset = 0
     for j in range(length - 1,-1,-1):
         desc = desc_level[j]
         fill_path_mapping(desc)
         pdesc = dm.pdesc(desc)
         leaf_handler(desc,pdesc)
-        #leaf_handler(desc,pdesc,offset)
-        #offset = offset + 1
     for i in range(depth-2,0,-1):
-        #offset = 0
         desc_level = desc_matrix[i]
         length = desc_level.__len__()
         for j in range(length-1,-1,-1):
@@ -3672,12 +3664,8 @@ def fullfill_descendants_info(desc_matrix):
             pdesc = dm.pdesc(desc)
             if(desc['leaf']):
                 leaf_handler(desc,pdesc)
-                #leaf_handler(desc,pdesc,offset)
-                #offset = offset + 1
             else:
                 non_leaf_handler(desc,pdesc)
-                #non_leaf_handler(desc,pdesc,offset)
-                #offset = offset + desc['flat_len']
     desc_matrix[0][0]['flat_offset'] = (0,desc_matrix[0][0]['flat_len'])
     for i in range(0,depth-1):
         pdesc_level = desc_matrix[i]
