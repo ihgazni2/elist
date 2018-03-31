6e6d89717e8ff3137c60ca58d74b08c2944e7c61
ihgazni2 <286264978@qq.com>
2018-04-01 00:49:39 +0800

    remove old build

--- elist/elist.py
+++ elist/elist.py
@@ -2838,11 +2838,23 @@ def join(ol,separator=","):
         ol = [1,2,3,4]
         join(ol,separator="-")
     '''
-    rslt =""
+    cond = (type(ol[0])==type(b''))
+    if(cond):
+        rslt = b''
+    else:
+        rslt =""
     length = ol.__len__()
     for i in range(0,length-1):
-        rslt = rslt + str(ol[i]) + separator
-    rslt = rslt + str(ol[length - 1])
+        ele = ol[i]
+        if(cond):
+            pass
+        else:
+            ele = str(ele)
+        rslt = rslt + ele + separator
+    if(cond):
+        rslt = rslt + ol[length - 1]
+    else:
+        rslt = rslt + str(ol[length - 1])
     return(rslt)
 
 def join2(ol,*sps):
