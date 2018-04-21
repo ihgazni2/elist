42cb97ea914a9441b561028f8ab30b7e4e868cce
ihgazni2 <286264978@qq.com>
2018-04-21 19:28:26 +0800

    remove old build

--- elist/elist.py
+++ elist/elist.py
@@ -3191,6 +3191,47 @@ def filter(ol,test_func,*args,**kwargs):
         ol.extend(new)
         return(ol)
 
+##############################
+def filter2(ol,test_func,*args,**kwargs):
+    '''
+        from elist.elist import *
+        def test_func(ele,index,x):
+            cond1 = (ele > x)
+            cond2 = (index % 2 = 1)
+            cond = (cond1 & cond2)
+            return(cond)
+        ol = [1,2,3,4,5]
+        id(ol)
+        new = filter2(ol,test_func,2)
+        new
+        id(new)
+        #####
+        ol = [10,20,30,40]
+        id(ol)
+        rslt = filter2(ol,test_func,3,mode="original")
+        rslt
+        id(rslt)
+    '''
+    if('mode' in kwargs):
+        mode = kwargs['mode']
+    else:
+        mode = "new"
+    length = ol.__len__()
+    new = []
+    cpol = copy.deepcopy(ol)
+    for i in range(0,length):
+        cond = test_func(cpol[i],i,*args)
+        if(cond):
+            new.append(cpol[i])
+        else:
+            pass
+    if(mode == "new"):
+        return(new)
+    else:
+        ol.clear()
+        ol.extend(new)
+        return(ol)
+
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
