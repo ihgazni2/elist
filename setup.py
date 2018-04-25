3c89b0863a715d8d75a841380e9fe28ea89d2a20
ihgazni2 <286264978@qq.com>
2018-04-25 23:55:39 +0800

    remove old build

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name="elist",
-      version = "0.3.15",
+      version = "0.3.16",
       description="handle list,nested list tree",
       author="dapeli",
       url="https://github.com/ihgazni2/elist",
