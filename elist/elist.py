3a978684ed046af706e0e0a57dda9c4ef988f11d
ihgazni2 <286264978@qq.com>
2018-04-25 14:04:59 +0800

    remove old build

--- elist/elist.py
+++ elist/elist.py
@@ -70,7 +70,7 @@ def cond_select_all(ol,**kwargs):
 def cond_select_all2(ol,**kwargs):
     '''
         from elist.elist import *
-        from elist.jprint import pobj
+        from xdict.jprint import pobj
         def test_func(ele,index,x):
             cond1 = (ele > x)
             cond2 = (index %2 == 0)
@@ -78,7 +78,7 @@ def cond_select_all2(ol,**kwargs):
             return(cond)
 
         ol = [1,2,3,4,5,6,7]
-        rslt = cond_select_all(ol,cond_func = test_func,cond_func_args = [3])
+        rslt = cond_select_all2(ol,cond_func = test_func,cond_func_args = [3])
         pobj(rslt)
     '''
     cond_func = kwargs['cond_func']
@@ -120,6 +120,29 @@ def cond_select_indexes_all(ol,**kwargs):
     return(rslt)
 
 
+def cond_select_indexes_all2(ol,**kwargs):
+    '''
+        from elist.elist import *
+        from xdict.jprint import pobj
+        def test_func(ele,index,x):
+            cond1 = (ele > x)
+            cond2 = (index %2 == 0)
+            cond =(cond1 & cond2)
+            return(cond)
+
+        ol = [1,2,3,4,5,6,7]
+        rslt = cond_select_indexes_all2(ol,cond_func = test_func,cond_func_args = [3])
+        pobj(rslt)
+    '''
+    cond_func = kwargs['cond_func']
+    if('cond_func_args' in kwargs):
+        cond_func_args = kwargs['cond_func_args']
+    else:
+        cond_func_args = []
+    ####
+    founded = find_all2(ol,cond_func,*cond_func_args)
+    rslt = array_map(founded,lambda ele:ele['index'])
+    return(rslt)
 
 
 
