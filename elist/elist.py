bcbfcd6b7c6806b8f3c10a0ac83703fa1848c221
dli <terryinzaghi@163.com>
2018-11-28 11:39:36 +0000

    remove old build

--- elist/elist.py
+++ elist/elist.py
@@ -134,6 +134,11 @@ def cond_select_all2(ol,**kwargs):
     return(rslt)
 
 
+
+#def icond_select_all(ol,**kwargs):
+
+
+
 cond_select_values_all = cond_select_all
 cond_select_values_all2 = cond_select_all2
 
@@ -3242,6 +3247,23 @@ def interleave(*arrays,**kwargs):
             array = arrays[i]
             rslt.append(array[j])
     return(rslt)
+
+
+def deinterleave(ol,gnum):
+    '''
+        ol = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        deinterleave(ol,3)
+        
+    '''
+    def test_func(ele,index,interval,which):
+        cond= (index % interval == which)
+        return(cond)
+    rslt = []
+    for i in range(0,gnum):
+        arr = cond_select_all2(ol,cond_func = test_func,cond_func_args = [gnum,i])
+        rslt.append(arr)
+    return(rslt)
+
 
 #@@@@@@@@@@@@@@@@@@
 
